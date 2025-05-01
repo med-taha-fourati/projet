f36da2a6c44e0b6adc87a601517936901f32c841
trbshyguy1010 <taha.fourati2005@gmail.com>
2025-05-01 15:29:31 +0100

    another styling commit

--- Vues/Authentification.php
+++ Vues/Authentification.php
@@ -4,17 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion de reparation d'ordinateurs</title>
+    <link rel="stylesheet" href="./Styles/auth_style.css">
 </head>
 <body>
-    <h1>Login - Gestion de reparation d'ordinateurs</h1>
-    <form action="../Controlleur/AuthController.php" method="post">
-        <label for="login">Login:</label>
-        <input type="text" id="login" name="loginL" required><br><br>
+    <div class="the-whole-thing">
+        <div class="the-image">
+            <div class="winder">
+                <h1>Gestion de reparation d'ordinateurs</h1>
+            </div>
+        </div>
+        <div class="the-form">
+            <h1>Login</h1>
+            <form action="../Controlleur/AuthController.php" method="post">
+                <label for="login">Login:</label>
+                <input type="text" id="login" name="loginL" required><br><br>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="passwordL" required><br><br>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="passwordL" required><br><br>
 
-        <input type="submit" name="login_btn" value="Login">
-    </form>
+                <input type="submit" name="login_btn" value="Login">
+            </form>
+            <a href="../Vues/InscriptionUtilisateur.php">Pas de compte?</a>
+        </div>
+    </div>
 </body>
 </html>
--- Vues/InscriptionUtilisateur.php
+++ Vues/InscriptionUtilisateur.php
@@ -4,30 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion de reparation d'ordinateurs</title>
+    <link rel="stylesheet" href="./Styles/auth_style.css">
 </head>
 <body>
-    <h1>Inscription - Gestion de reparation d'ordinateurs</h1>
-    <form action="../Controlleur/AuthController.php" method="post">
-        <label for="login">Login:</label>
-        <input type="text" id="login" name="login" required><br><br>
+    <div class="the-whole-thing">
+        <div class="the-image">
+            <div class="winder">
+                <h1>Gestion de reparation d'ordinateurs</h1>
+            </div>
+        </div>
+        <div class="the-form">
+            <h1>S'inscrire</h1>
+            <form action="../Controlleur/AuthController.php" method="post">
+                <label for="login">Login:</label>
+                <input type="text" id="login" name="login" required><br><br>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required><br><br>
 
-        <label for="nom">Nom:</label>
-        <input type="text" id="nom" name="nom" required><br><br>
+                <label for="nom">Nom:</label>
+                <input type="text" id="nom" name="nom" required><br><br>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
-        
-        <label for="adresse">Adresse:</label>
-        <input type="text" id="adresse" name="adresse" required><br><br>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required><br><br>
 
-        <label for="tel">Telephone:</label>
-        <input type="number" min="10000000" max="99999999" id="tel" name="tel" required><br><br>
-        
-        <input type="submit" name="inscription" value="Inscription">
-        <input type="reset" value="Annuler">
-    </form>
+                <label for="adresse">Adresse:</label>
+                <input type="text" id="adresse" name="adresse" required><br><br>
+
+                <label for="tel">Telephone:</label>
+                <input type="number" min="10000000" max="99999999" id="tel" name="tel" required><br><br>
+
+                <div class="action-buttons">
+                    <input type="reset" value="Reinitialiser">
+                    <input type="submit" name="inscription" value="Inscription">
+                
+                </div>
+            </form>
+            <a href="../Vues/Authentification.php">Avez vous deja un compte?</a>
+        </div>
+    </div>
+    
 </body>
 </html>
